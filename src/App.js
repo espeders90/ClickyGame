ef5de38f2550c9766dbff2b836636470799739f9
Eddie Pedersen <eddiepedersen90@gmail.com>
2018-01-13 01:30:42 +0000

    major work needs to be done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/bootstrap/navbar.js';
-import Body from './components/bootstrap/body.js';
-
-//create comparative function (reset or total one)
-
-
-//randomize pictures function
-
+import React, { Component } from "react";
+import Car from "./components/Car";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import cars from "./cars.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.friends to the cars json array
+  state = {
+    cars
+  };
+
+  removeCar = id => {
+    // Filter this.state.car for car with an id not equal to the id being removed
+    const cars = this.state.cars.filter(cars => cars.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ cars });
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Body />
-      </div>
+      <Wrapper>
+        <Title>Cars List</Title>
+        {this.state.cars.map(car => (
+          <Car
+            removeCar={this.removeCar}
+            id={Car.id}
+            key={Car.id}
+            name={Car.name}
+            image={Car.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
